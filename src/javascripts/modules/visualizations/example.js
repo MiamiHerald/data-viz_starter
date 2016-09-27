7592f817e19d25b7972628fa9f8dba76eb83982b
Chris Williams <cwilliams@miamiherald.com>
2016-09-27 13:07:30 -0400

    Added javascript dependencies

--- src/javascripts/modules/visualizations/example.js
+++ src/javascripts/modules/visualizations/example.js
@@ -1,5 +1,9 @@
+import $ from 'jquery';
+import * as d3 from 'd3';
+import * as topojson from 'topojson';
+
 const loadExample = () => {
-  console.log(`loaded`);
+  console.log($, d3, topojson);
 }
 
 export { loadExample };
