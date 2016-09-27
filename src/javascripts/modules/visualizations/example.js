71098f7a59186f721de84179b6a2cb8ca56393cd
Chris Williams <cwilliams@miamiherald.com>
2016-09-27 18:06:20 -0400

    Added pym.js

--- src/javascripts/modules/visualizations/example.js
+++ src/javascripts/modules/visualizations/example.js
@@ -1,9 +1,10 @@
 import $ from 'jquery';
 import * as d3 from 'd3';
 import * as topojson from 'topojson';
+import * as pym from 'pym.js'
 
 const loadExample = () => {
-  console.log($, d3, topojson);
+  console.log($, d3, topojson, pym);
 }
 
 export { loadExample };
